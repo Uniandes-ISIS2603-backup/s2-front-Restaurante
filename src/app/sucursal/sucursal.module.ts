bcbfe5051299f70e34a0a5be683af4b1c20902cd
Juliana Prieto <jprietoarcila@gmail.com>
2018-10-18 08:12:49 -0500

    Creado el modulo y componente sucursal-list

---
+++ src/app/sucursal/sucursal.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SucursalListComponent } from './sucursal-list/sucursal-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [SucursalListComponent]
+})
+export class SucursalModule { }
