082a272129c22e9675249da0893a8c5227390586
Juan Ortega <ja.ortega@uniandes.edu.co>
2018-10-17 19:24:48 -0500

    Creado el módulo reserva y el componente reserva-list

---
+++ src/app/reserva/reserva.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReservaListComponent } from './reserva-list/reserva-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ReservaListComponent]
+})
+export class ReservaModule { }
