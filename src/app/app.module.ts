1d560fc338cdd296519ad33adf9082acc3a23a61
Juan Ortega <ja.ortega@uniandes.edu.co>
2018-11-08 14:06:48 -0500

    ErrorInterceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpErrorInterceptor } from './interceptors/httperrorinterceptor.service';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { ToastrModule } from 'ngx-toastr';
+import { ToastrService } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ClienteModule } from './cliente/cliente.module';
 import { ReservaModule } from './reserva/reserva.module';
@@ -13,7 +15,10 @@ import { DomicilioModule } from './domicilio/domicilio.module';
 import { MesaModule } from './mesa/mesa.module';
 import { CalificacionModule } from './calificacion/calificacion.module';
 import { TarjetaModule } from './tarjeta/tarjeta.module';
-
+import { NgxPaginationModule } from 'ngx-pagination';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ModalDialogModule } from 'ngx-modal-dialog';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 @NgModule({
   declarations: [
@@ -36,10 +41,19 @@ import { TarjetaModule } from './tarjeta/tarjeta.module';
     DomicilioModule,
     MesaModule,
     TarjetaModule,
-    CalificacionModule
+    CalificacionModule,
+    BrowserAnimationsModule,
+    NgxPaginationModule,
+    NgbModule,
+    ModalDialogModule,
+    AngularFontAwesomeModule
   ],
   providers: [
-
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpErrorInterceptor,
+      multi: true
+    }
   ],
   bootstrap: [AppComponent]
 })
