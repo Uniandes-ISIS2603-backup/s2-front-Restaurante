381ad254bbf5d0420cc353ade58fc62ff05f6fde
Juan Ortega <ja.ortega@uniandes.edu.co>
2018-11-29 00:59:13 -0500

    merge arreglado

--- src/app/sucursal/sucursal.module.ts
+++ src/app/sucursal/sucursal.module.ts
@@ -9,12 +9,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { SucursalCreateComponent } from './sucursal-create/sucursal-create.component';
 import { FormsModule } from '@angular/forms';
-<<<<<<< HEAD
 import {NgxPermissionsModule} from 'ngx-permissions';
 
-=======
 import {SucursalEditComponent} from './sucursal-edit/sucursal-edit.component';
->>>>>>> de59162ea80c7b07120045cfd1973e4e6f6fd9ac
+
 
 @NgModule({
   imports: [
